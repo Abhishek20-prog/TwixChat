50fcf6902ad78ecfbe2dbf7ee6a5ca29516e6f7f
Abhishek kumar <abhishekkumastudent1234@gmail.com>
2026-08-29 22:01:11 +0530

    hello

--- server/controllers/usercontrollers.js
+++ server/controllers/usercontrollers.js
@@ -1,3 +1,5 @@
+import transformationUtils from "@imagekit/nodejs/lib/transformation-utils.js";
+import imagekit from "../config/imagekit";
 import User from "../models/user";
 
 export const getUser= async (res,req) => {
@@ -39,6 +41,44 @@ export const updateUser= async (res,req) => {
     }
     const profile = req.files.profile && req.files.profile [0]
     const cover = req.files.cover && req.files.cover [0]
+    if (cover) {
+    const buffer = fs.readFileSync(cover.path)
+
+    const response = await imagekit.upload({
+        file: buffer,
+        fileName: cover.originalname,
+    })
+    const url = imagekit.url({
+        path:response.filepath,
+        transformation:[
+            {quality:'auto'},
+            {format:'webp'},
+            {width:'1280'}
+
+        ]
+    })
+    updatedData.cover_photo = url;
+}
+if (profile) {
+    const buffer = fs.readFileSync(profile.path)
+
+    const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+    })
+    const url = imagekit.url({
+        path:response.filepath,
+        transformation:[
+            {quality:'auto'},
+            {format:'webp'},
+            {width:'512'}
+
+        ]
+    })
+    updatedData.profile_picture = url;
+}
+const user = await User.findByIdAndUpdate(userID , updatedData ,{new:true})
+res.json({success:true , user , message:"profile created successfully"})
     
 
     } catch (error) {
