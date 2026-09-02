3c1a5a3ed41c58e6915d8b138421b73dd7ea38a0
Abhishek kumar <abhishekkumastudent1234@gmail.com>
2026-09-02 20:43:22 +0530

    hello

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,14 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { getUserData } from '../controllers/usercontrollers.js';
+import { upload } from '../middleware/multer.js';
+import { getUser, updateUser ,discoveruser} from '../controllers/usercontrollers.js';
 const userRouter = express.Router();
-userRouter.get('/data',protect, getUserData);
+userRouter.get('/data',protect, getUser);
+userRouter.post('/data',upload.fields([{ name: 'cover', maxCount: 1 }, { name: 'profile', maxCount: 1 }]), protect, updateUser);
+userRouter.post('/discover',protect, discoveruser);
+userRouter.post('/follow',protect, followuser);
+userRouter.post('/unfollow',protect, unfollowuser);
+
+
+
+
